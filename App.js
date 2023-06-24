3aaef88fa9e876e63ffebebdb45994ae22adc3ea
RobertBirlan <112295018+qbRobert@users.noreply.github.com>
2023-06-24 13:37:01 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Exchange from './Components/Exchange/Exchange';
+function App() {
+  return (
+    <div className="App">
+      <div className="App-container">
+        <h2>Exchange Rates</h2>
+        <Exchange />
+      </div>
+    </div>
+  );
+}
+
+export default App;
